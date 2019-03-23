d85a38a10d6cea9d7c0f78b234c4dde0b5c923e4
PNN11 <47753604+PNN11@users.noreply.github.com>
2019-03-23 10:27:24 +0300

    feat: update function getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -5,17 +5,18 @@
 module.exports = function getLoveTrianglesCount(preferences = []) {
     let count = 0;
     let arr = preferences;
+    arr.unshift(0)
 
     for (let i=0; i<arr.length; i++) {
         let first = arr[i];
-        let second = arr[first-1];
-        let third = arr[second-1];
+        let second = arr[first];
+        let third = arr[second];
 
-        if (third == i+1 && arr[i]!== i+1) {
+        if (third == i && arr[i]!== i) {
             count++;
             arr[i] = -1;
-            arr[first-1] = -1;
-            arr[second-1] = -1;
+            arr[first] = -1;
+            arr[second] = -1;
         }
     }
     return count;
